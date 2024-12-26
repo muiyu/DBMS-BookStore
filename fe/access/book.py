c41a9767b0ebbaa534a1a93a0880e38e7d6f9ab1
Muiyu <you@example.com>
2024-12-26 15:37:51 +0000

    Final version

--- fe/access/book.py
+++ fe/access/book.py
@@ -30,8 +30,8 @@ class Book:
 class BookDB:
     def __init__(self, large: bool = False):
         parent_path = os.path.dirname(os.path.dirname(__file__))
-        self.db_s = os.path.join(parent_path, "data/book.db")
-        self.db_l = os.path.join(parent_path, "data/book_lx.db")
+        self.db_s = os.path.join(parent_path, "/root/course-lab/Con-DataM-Sys/DBMS-BookStore/book.db")
+        self.db_l = os.path.join(parent_path, "/root/course-lab/Con-DataM-Sys/DBMS-BookStore/book_lx.db")
         if large:
             self.book_db = self.db_l
         else:
